66f90a1f85b3f63f636467d480ce52bca4a60423
VaishnaviRajput <rajputvaishnavikaransing10@gmail.com>
2024-07-03 21:12:15 +0530

    new

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,33 @@
-import React from "react";
-import "./../styles/App.css";
-import UserInfo from "./UserInfo";
 
+import React, { useState } from "react";
+import './../styles/App.css';
+import { useDispatch, useSelector } from "react-redux";
+import { dataNameFn,dataMailFn } from "../redux/reducer";
 const App = () => {
+  const [name,setName] = useState('');
+  const [email,setEmail] = useState('');
+  const myState = useSelector(state => state.data)
+  console.log(myState);
+const dispatch = useDispatch();
+  function handleName(e){
+    setName(e.target.value)
+  dispatch(dataNameFn(e.target.value));
+  }
+
+  function handleEmail(e){
+    setEmail(e.target.value)
+  dispatch(dataMailFn(e.target.value));
+  }
   return (
     <div>
-      <UserInfo />
+      <h1>User Information</h1>
+       <div>Name : <input type="text" onChange={handleName}/></div>
+       <div>Email : <input type="email" onChange={handleEmail}/></div>
+       <div>Current values in store : </div>
+       <div className="output">Name - {myState.name}</div>
+       <div className="output">Email - {myState.email}</div>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
